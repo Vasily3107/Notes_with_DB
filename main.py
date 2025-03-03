ea0fdc8629d24c4915e85fc92026cada9a39c468
Vasily3107 <vasilynelis@gmail.com>
2025-03-03 15:35:31 +0200

    Updated main.py | Created console user interface
    alailable options for user:
     - view all notes
     - add a note
     - delete a note
     - exit the app

--- main.py
+++ main.py
@@ -1,1 +1,55 @@
+from Note import Note
+from NoteHandler import NoteHandler
 
+server = 'localhost\SQLEXPRESS'
+database = 'test_db'
+table = 'notes'
+
+nh = NoteHandler(server, database, table)
+
+from os import system
+def cls(): system("cls")
+def wait(): input("\nPress \"Enter\" to continue...")
+
+while True:
+    cls()
+    print("Main menu: \n")
+
+    print("1 - View all notes")
+    print("2 - Add note")
+    print("3 - Delete note")
+    print("4 - Exit \n")
+
+    match input("Enter choice: "):
+        case "1":
+            cls()
+            print("Notes:")
+            for i in nh.get_notes(): print(i)
+            wait()
+
+
+        case "2":
+            cls()
+            print("Adding note: \n")
+
+            name = input("Enter note name: ")
+            text = input("     Enter text: ")
+
+            cls()
+            print(nh.add_note(Note(name, text)))
+            wait()
+
+
+        case "3":
+            cls()
+            print("Deleting note: \n")
+
+            name = input("Enter note name: ")
+
+            cls()
+            print(nh.del_note(name))
+            wait()
+
+
+        case "4": break
+        case   _: pass
